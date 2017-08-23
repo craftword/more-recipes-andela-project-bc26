c2a60f6495080972404c1c3ef2787dd8eae217f9
Abimbola Olaitan <craftwordltd@gmail.com>
2017-08-23 13:02:16 +0100

    modify package json

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,19 +1,19 @@
-let gulp = require('gulp');
-let sass = require('gulp-sass');
+import gulp from "gulp";
+import sass from "gulp-sass";
 
-gulp.task('styles', ()=> {
-    gulp.src('template/sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./template/css/'));
+gulp.task("styles", ()=> {
+    gulp.src("template/sass/**/*.scss")
+        .pipe(sass().on("error", sass.logError))
+        .pipe(gulp.dest("./template/css/"));
 });
 
 
 //Watch task
-gulp.task('default',()=> {
-    gulp.watch('template/sass/**/*.scss',['styles']);
+gulp.task("default",()=> {
+    gulp.watch("template/sass/**/*.scss",["styles"]);
 });
 
-gulp.task('travis',  ()=> {
-    gulp.watch('test/testRoute.js')
+gulp.task("travis",  ()=> {
+    gulp.watch("test/testRoute.js");
     process.exit(0);
 });
