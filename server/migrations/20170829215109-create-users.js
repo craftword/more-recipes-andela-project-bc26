fb2cb11e646618ff9417c0608707c728f2fbc8a2
Abimbola Olaitan <craftwordltd@gmail.com>
2017-08-30 11:07:05 +0100

    modify users and recipes migration

--- server/migrations/20170829215109-create-users.js
+++ server/migrations/20170829215109-create-users.js
@@ -1,27 +1,57 @@
-'use strict';
 module.exports = {
-  up: function(queryInterface, Sequelize) {
-    return queryInterface.createTable('Users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      username: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: function(queryInterface, Sequelize) {
-    return queryInterface.dropTable('Users');
-  }
+    up: (queryInterface, Sequelize) => 
+        queryInterface.createTable("Users", {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            username: {
+                type: Sequelize.STRING,
+                unique: true,
+                allowNull: false
+            },
+            password: {
+                allowNull: false,
+                type: Sequelize.STRING
+            },
+            fullname: {
+                allowNull: false,
+                type: Sequelize.STRING
+            },
+            email: {
+                allowNull: false,
+                type: Sequelize.STRING
+            },
+            phone: {
+                allowNull: false,
+                type: Sequelize.STRING
+            },
+            picture: {
+                allowNull: false,
+                type: Sequelize.STRING
+            },
+            address: {
+                allowNull: false,
+                type: Sequelize.STRING
+            },
+            state: {
+                allowNull: false,
+                type: Sequelize.STRING
+            },
+            country: {
+                allowNull: false,
+                type: Sequelize.STRING
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+        }),  
+    down: (queryInterface /* , Sequelize */) => queryInterface.dropTable("Users"),
 };
--- server/migrations/20170830091151-create-recipes.js
+++ server/migrations/20170830091151-create-recipes.js
@@ -1,27 +1,49 @@
-'use strict';
+"use strict";
 module.exports = {
-  up: function(queryInterface, Sequelize) {
-    return queryInterface.createTable('Recipes', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      name: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: function(queryInterface, Sequelize) {
-    return queryInterface.dropTable('Recipes');
-  }
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable("Recipes", {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            name: {
+                allowNull: false,
+                type: Sequelize.STRING
+            },
+            description: {
+                allowNull: false,
+                type: Sequelize.TEXT
+            },
+            ingredients: {
+                allowNull: false,
+                type: Sequelize.TEXT
+            },
+            directions: {
+                allowNull: false,
+                type: Sequelize.TEXT
+            },
+            userId: {
+                type: Sequelize.INTEGER,
+                onDelete: "CASCADE",
+                references: {
+                    model: "Users",
+                    key: "id",
+                    as: "userId",
+                },
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: function(queryInterface, Sequelize) {
+        return queryInterface.dropTable('Recipes');
+    }
 };
